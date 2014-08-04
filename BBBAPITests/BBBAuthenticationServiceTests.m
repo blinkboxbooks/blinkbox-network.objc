60016ed1080a77b462c23c073f6529eaa5e6f92c
Owen Worley <owen@blinkbox.com>
2014-08-04 15:06:05 +0100

    Add BBBAuthenticationServiceTests file

---
+++ BBBAPITests/BBBAuthenticationServiceTests.m
@@ -0,0 +1,34 @@
+//
+//  BBBAuthenticationServiceTests.m
+//  BBBAPI
+//
+//  Created by Owen Worley on 04/08/2014.
+//  Copyright (c) 2014 Blinkbox Books. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BBBAuthenticationServiceTests : XCTestCase
+
+@end
+
+@implementation BBBAuthenticationServiceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
