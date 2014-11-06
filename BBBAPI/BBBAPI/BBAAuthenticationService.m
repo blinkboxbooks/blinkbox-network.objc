0e2aada23a6f84e0063d832948b51c09a0528d22
Tomasz Kuźma <tomek@blinkbox.com>
2014-11-06 11:14:13 +0000

    Not touching properties in the init in the auth service

--- BBBAPI/BBBAPI/BBAAuthenticationService.m
+++ BBBAPI/BBBAPI/BBAAuthenticationService.m
@@ -28,9 +28,9 @@
     if (self = [super init]) {
         BBANetworkConfiguration *configuration;
         configuration = [BBANetworkConfiguration defaultConfiguration];
-        self.authResponseMapper = [configuration newResponseMapperForServiceName:kBBAAuthServiceName];
-        self.tokensResponseMapper = [configuration newResponseMapperForServiceName:kBBAAuthServiceTokensName];
-        self.clientsResponseMapper = [configuration newResponseMapperForServiceName:kBBAAuthServiceClientsName];
+        _authResponseMapper = [configuration newResponseMapperForServiceName:kBBAAuthServiceName];
+        _tokensResponseMapper = [configuration newResponseMapperForServiceName:kBBAAuthServiceTokensName];
+        _clientsResponseMapper = [configuration newResponseMapperForServiceName:kBBAAuthServiceClientsName];
 
         BBALog(@"AuthenticationService Initialised");
     }
