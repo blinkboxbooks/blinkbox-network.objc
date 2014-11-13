bb59f758972916d19fe696bfc9abafd774dfaa03
Tomasz Kuźma <tomek@blinkbox.com>
2014-11-13 11:10:13 +0000

    Generated BBBTool project again
    Added clean workspace

---
+++ BBBAPI/BBBAPI/Classes/BBBAPI.h
@@ -0,0 +1,19 @@
+//
+//  BBBAPI.h
+//  BBBAPI
+//
+//  Created by Tomek Kuźma on 13/11/2014.
+//  Copyright (c) 2014 Blinkbox Entertainment Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BBAAuthenticationService.h"
+#import "BBALibraryService.h"
+#import "BBALibraryItem.h"
+#import "BBAUserDetails.h"
+#import "BBAClientDetails.h"
+#import "BBAAuthData.h"
+
+@interface BBBAPI : NSObject
+
+@end
