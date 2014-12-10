4fcfc1e1b9a793f717ee21a9e74ac8a3de725f46
Owen Worley <owen@blinkbox.com>
2014-12-10 16:29:41 +0000

    ResponseMapper documentation

--- BBBAPI/BBBAPI/Classes/BBABookmarkResponseMapper.h
+++ BBBAPI/BBBAPI/Classes/BBABookmarkResponseMapper.h
@@ -18,17 +18,46 @@ typedef NS_ENUM(NSUInteger, BBABookmarkResponseMapperType) {
     BBABookmarkResponseMapperTypeUpdateBookmark = 5,
 };
 
+/**
+ *  Represents a response from the Bookmark service, contianing an `NSArray` of bookmarks and (optionally) a lastSyncDate
+ */
 @interface BBABookmarkResponse : NSObject
 @property (nonatomic, copy) NSDate *lastSyncDate;
 @property (nonatomic, copy) NSArray *bookmarks;
 
+/**
+ *  Create a `BBABookmarkResponse` from `NSData` containing JSON Values
+ *
+ *  @param data `NSData` containing JSON Bookmark values
+ *
+ *  @return `BBABookmarkResponse`
+ */
 - (instancetype) initWithData:(NSData *)data;
 
 @end
 
+/**
+ *  `BBABookmarkResponseMapper` is responsible for mapping responses from the bookmark service into
+ *  a format usable by the API library.
+ */
 @interface BBABookmarkResponseMapper : NSObject <BBAResponseMapping>
 
+/**
+ *  Returns a mapper with a given type
+ *
+ *  @param type see `BBABookmarkResponseMapperType` for a list of valid types
+ *
+ *  @return `BBABookmarkResponseMapper` configured for mapping responses of the given `type`
+ */
 + (BBABookmarkResponseMapper *) mapperWithType:(BBABookmarkResponseMapperType)type;
+
+/**
+ *  Initialises a mapper with a given type
+ *
+ *  @param type see `BBABookmarkResponseMapperType` for a list of valid types
+ *
+ *  @return `BBABookmarkResponseMapper` configured for mapping responses of the given `type`
+ */
 - (instancetype) initWithType:(BBABookmarkResponseMapperType)type;
 
 @end
