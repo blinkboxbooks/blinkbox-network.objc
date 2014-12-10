3b4e4b689c32e9b95591db1283278731a849d9f8
Owen Worley <owen@blinkbox.com>
2014-12-10 16:12:36 +0000

    Add BookmarkResponseMapper code to map responses from Bookmark Service

--- BBBAPI/BBBAPI/Classes/BBABookmarkResponseMapper.m
+++ BBBAPI/BBBAPI/Classes/BBABookmarkResponseMapper.m
@@ -18,7 +18,13 @@
     if (self) {
         NSDictionary *bookmarksJSON;
         bookmarksJSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-        _bookmarks = [BBABookmarkItem bookmarkItemsWithJSONArray:bookmarksJSON[@"bookmarks"]];
+
+        NSArray *JSONBookmarks = bookmarksJSON[@"bookmarks"];
+        if (!JSONBookmarks && bookmarksJSON) {
+            JSONBookmarks = @[bookmarksJSON];
+        }
+
+        _bookmarks = [BBABookmarkItem bookmarkItemsWithJSONArray:JSONBookmarks];
 
         NSDate *date = [BBADateHelper dateFromString:bookmarksJSON[@"lastSyncDateTime"]];
         _lastSyncDate = date;
@@ -52,7 +58,7 @@
     return self;
 }
 
-- (BBABookmarkResponse *) responseFromData:(NSData *)data
+- (id) responseFromData:(NSData *)data
                response:(NSURLResponse *)response
                   error:(NSError *__autoreleasing *)error{
 
@@ -62,7 +68,7 @@
                            error:error];
 }
 
-- (BBABookmarkResponse *) responseForType:(BBABookmarkResponseMapperType)type
+- (id) responseForType:(BBABookmarkResponseMapperType)type
                   data:(NSData *)data
                response:(NSURLResponse *)response
                  error:(NSError *__autoreleasing *)error{
@@ -71,8 +77,25 @@
             return [self multipleBookmarksGETResponseWithData:data
                                                      response:response
                                                         error:error];
-            break;
 
+        case BBABookmarkResponseMapperTypeDeleteMultipleBookmarks:
+            return @([self multipleBookmarksDELETEResponseWithData:data
+                                                          response:response
+                                                             error:error]);
+
+        case BBABookmarkResponseMapperTypeDeleteBookmark:
+            return @([self singleBookmarkDELETEResponseWithData:data
+                                                       response:response
+                                                          error:error]);
+
+        case BBABookmarkResponseMapperTypeCreateBookmark:
+            return [self bookmarkCREATEResponseWithData:data
+                                               response:response
+                                                  error:error];
+        case BBABookmarkResponseMapperTypeUpdateBookmark:
+            return @([self bookmarkUPDATERResponseWithData:data
+                                               response:response
+                                                  error:error]);
         default:
             break;
     }
@@ -88,25 +111,120 @@
         BBABookmarkResponse *bookmarkResponse = [[BBABookmarkResponse alloc] initWithData:data];
         return bookmarkResponse;
     }
-    else if (httpResonse.statusCode == BBAHTTPBadRequest){
+    else{
+        [self setError:error fromStatusCode:httpResonse.statusCode];
+        return nil;
+    }
 
-        if (error) {
-            *error = [NSError errorWithDomain:BBAConnectionErrorDomain
-                                         code:BBAAPIBadRequest
-                                     userInfo:nil];
-            return nil;
-        }
-    }
-    else if(httpResonse.statusCode == BBAHTTPServerError){
-        if (error) {
-            *error = [NSError errorWithDomain:BBAConnectionErrorDomain
-                                         code:BBAAPIServerError
-                                     userInfo:nil];
-            return nil;
-        }
-    }
     return nil;
 }
 
+- (BOOL) multipleBookmarksDELETEResponseWithData:(NSData *)data
+                                        response:(NSURLResponse *)response
+                                           error:(NSError *__autoreleasing *)error{
+    NSHTTPURLResponse *httpResonse = (NSHTTPURLResponse *)response;
+
+
+    //204 - Success
+    if (httpResonse.statusCode == BBAHTTPNoContent /* 204 */) {
+        return YES;
+
+    }
+    else{
+        [self setError:error fromStatusCode:httpResonse.statusCode];
+        return NO;
+    }
+    return NO;
+}
+
+- (BOOL) singleBookmarkDELETEResponseWithData:(NSData *)data
+                                     response:(NSURLResponse *)response
+                                        error:(NSError *__autoreleasing *)error{
+    NSHTTPURLResponse *httpResonse = (NSHTTPURLResponse *)response;
+
+    if (httpResonse.statusCode == BBAHTTPNoContent /* 204 */) {
+        return YES;
+    }
+    else{
+        [self setError:error fromStatusCode:httpResonse.statusCode];
+        return NO;
+    }
+
+    return NO;
+}
+
+-(BBABookmarkResponse *) bookmarkCREATEResponseWithData:(NSData *)data
+                                               response:(NSURLResponse *)response
+                                                  error:(NSError *__autoreleasing *)error{
+    NSHTTPURLResponse *httpResonse = (NSHTTPURLResponse *)response;
+    if (httpResonse.statusCode == BBAHTTPCreated /* 201 */) {
+        BBABookmarkResponse *bookmarkResponse = [[BBABookmarkResponse alloc] initWithData:data];
+        return bookmarkResponse;
+    }
+    else{
+        [self setError:error fromStatusCode:httpResonse.statusCode];
+        return nil;
+    }
+
+}
+
+- (BOOL) bookmarkUPDATERResponseWithData:(NSData *)data
+                                             response:(NSURLResponse *)response
+                                                error:(NSError *__autoreleasing *)error{
+    NSHTTPURLResponse *httpResonse = (NSHTTPURLResponse *)response;
+    if (httpResonse.statusCode == BBAHTTPSuccess /* 200 */) {
+        return YES;
+    }
+    else{
+        [self setError:error fromStatusCode:httpResonse.statusCode];
+        return NO;
+    }
+
+}
+
+- (void) setError:(NSError *__autoreleasing *)error
+   fromStatusCode:(NSInteger)statusCode{
+    if (!error) {
+        return;
+    }
+    //200 - Not found
+    if (statusCode == BBAHTTPSuccess /* 200 */) {
+        *error = [NSError errorWithDomain:BBAConnectionErrorDomain
+                                     code:BBAAPIErrorNotFound
+                                 userInfo:nil];
+    }
+    //404 - Not Found
+    else if (statusCode == BBAHTTPNotFound /* 404 */) {
+        *error = [NSError errorWithDomain:BBAConnectionErrorDomain
+                                     code:BBAAPIErrorNotFound
+                                 userInfo:nil];
+    }
+    //403 - Forbidden/Unauthorised
+    else if (statusCode == BBAHTTPForbidden /* 403 */) {
+        *error = [NSError errorWithDomain:BBAConnectionErrorDomain
+                                     code:BBAAPIErrorForbidden
+                                 userInfo:nil];
+    }
+    //400 - Bad Request
+    else if (statusCode == BBAHTTPBadRequest /* 400 */) {
+        *error = [NSError errorWithDomain:BBAConnectionErrorDomain
+                                     code:BBAAPIErrorBadRequest
+                                 userInfo:nil];
+
+    }
+    //409 - Conflict
+    else if (statusCode == BBAHTTPConflict /* 409 */) {
+        *error = [NSError errorWithDomain:BBAConnectionErrorDomain
+                                     code:BBAAPIErrorConflict
+                                 userInfo:nil];
+
+    }
+    //500 - Internal server error
+    else if (statusCode == BBAHTTPServerError /* 500 */) {
+        *error = [NSError errorWithDomain:BBAConnectionErrorDomain
+                                     code:BBAAPIServerError
+                                 userInfo:nil];
+    }
+}
 
 @end
