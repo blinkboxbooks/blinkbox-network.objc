c6ba73955f5042b7ac5279d25ce4bc0a14c2fa82
Owen Worley <owen@blinkbox.com>
2014-12-10 16:20:22 +0000

    Documentation in BookmarkService

--- BBBAPI/BBBAPI/Classes/BBABookmarkService.h
+++ BBBAPI/BBBAPI/Classes/BBABookmarkService.h
@@ -23,30 +23,75 @@ extern NSString *const kBBABookmarkServiceErrorDomain;
 @class BBABookmarkItem;
 @class BBAUserDetails;
 
+/**
+ *  The BBABookmarkService provides methods to fetch, create, delete and update bookmarks on the server.
+ */
 @interface BBABookmarkService : NSObject
 
+/**
+ *  The class to use for communication with the server. This defaults to BBAConnection.
+ */
 @property (nonatomic, strong) Class connectionClass;
 
+/**
+ *  Fetch a list of bookmarks from the server.
+ *
+ *  @param item       The `BBALibraryItem` for which to fetch bookmarks. (Required)
+ *  @param date       An optional `NSDate` which, if specified, will limit the returned bookmarks to those created AFTER this date.
+ *  @param types      `BBABookmarkType` bitmask specifying which bookmarks to fetch (Required)
+ *  @param user       `BBAUserDetails` specifying which user to fetch bookmarks for (Required)
+ *  @param completion Block invoked on completion of the fetch (Required)
+ */
 - (void) getBookmarkChangesForItem:(BBALibraryItem *)item
                          afterDate:(NSDate *)date
                          typesMask:(BBABookmarkType)types
                               user:(BBAUserDetails *)user
                         completion:(void (^)(NSArray *bookmarkChanges, NSDate *syncDate, NSError *error))completion;
 
+/**
+ *  Delete all bookmarks from the server for this book.
+ *
+ *  @param item       The `BBALibraryItem` for which to delete bookmarks (Required)
+ *  @param types      `BBABookmarkType` bitmask specifying which bookmarks to fetch (Required)
+ *  @param user       `BBAUserDetails` specifying which user to fetch bookmarks for (Required)
+ *  @param completion Block invoked on completion of the fetch (Required)
+ */
 - (void) deleteBookmarksForItem:(BBALibraryItem *)item
                       typesMask:(BBABookmarkType)types
                            user:(BBAUserDetails *)user
                      completion:(void (^)(BOOL success, NSError *error))completion;
 
+/**
+ *  Delete a bookmark from the server.
+ *
+ *  @param item       The `BBABookmarkItem` which to delete (Required)
+ *  @param user       `BBAUserDetails` specifying which user to delete bookmarks for (Required)
+ *  @param completion Block invoked on completion of the fetch (Required)
+ */
 - (void) deleteBookmark:(BBABookmarkItem *)item
                    user:(BBAUserDetails *)user
              completion:(void (^)(BOOL success, NSError *error))completion;
 
+/**
+ *  Create a bookmark on the server.
+ *
+ *  @param bookmark   `BBABookmarkItem` to create on the server. (Required)
+ *  @param item       `BBALibraryItem` representing the book that these bookmarks are part of (Required)
+ *  @param user       `BBAUserDetails` specifying which user to create the bookmark for (Required)
+ *  @param completion Block invoked on completion of the fetch (Required)
+ */
 - (void) addBookMark:(BBABookmarkItem *)bookmark
              forItem:(BBALibraryItem *)item
                 user:(BBAUserDetails *)user
           completion:(void (^)(BBABookmarkItem *bookmarkItem, NSError *error))completion;
 
+/**
+ *  Update a bookmark on the server, for example, changing the colour of a higlight.
+ *
+ *  @param item   `BBABookmarkItem` to update on the server. (Required)
+ *  @param user       `BBAUserDetails` specifying which user to update the bookmark for (Required)
+ *  @param completion Block invoked on completion of the fetch (Required)
+ */
 - (void) updateBookMark:(BBABookmarkItem *)item
                    user:(BBAUserDetails *)user
              completion:(void (^)(BOOL success, NSError *error))completion;
