11098bc55159ef3a2eacc62e61a9d27fcb49be3b
Tomasz Kuźma <tomek@blinkbox.com>
2015-02-06 10:36:08 +0000

    Removed unused files

--- BBBAPI/BBBAPI/Classes/BBBAPI.h
+++ BBBAPI/BBBAPI/Classes/BBBAPI.h
@@ -6,22 +6,22 @@
 //  Copyright (c) 2014 Blinkbox Entertainment Ltd. All rights reserved.
 //
 //
-//#import <BBBAPI/BBABookmarkService.h>
-//#import <BBBAPI/BBABookmarkItem.h>
-//#import <BBBAPI/BBALibraryService.h>
-//#import <BBBAPI/BBALibraryItem.h>
-//#import <BBBAPI/BBAAPIErrors.h>
-//#import <BBBAPI/BBAAuthenticationService.h>
-//#import <BBBAPI/BBAConnection.h>
-//#import <BBBAPI/BBACatalogueService.h>
-//#import <BBBAPI/BBABookItem.h>
-//#import <BBBAPI/BBAKeyService.h>
-//#import <BBBAPI/BBAAuthenticationServiceConstants.h>
-//#import <BBBAPI/BBAUserDetails.h>
-//#import <BBBAPI/BBAClientDetails.h>
-//#import <BBBAPI/BBAAuthData.h>
-//#import <BBBAPI/BBALinkItem.h>
-//#import <BBBAPI/BBAImageItem.h>
-//#import <BBBAPI/BBASearchService.h>
-//#import <BBBAPI/BBASuggestionItem.h>
-//#import <BBBAPI/BBASearchItem.h>
+#import <BBBAPI/BBABookmarkService.h>
+#import <BBBAPI/BBABookmarkItem.h>
+#import <BBBAPI/BBALibraryService.h>
+#import <BBBAPI/BBALibraryItem.h>
+#import <BBBAPI/BBAAPIErrors.h>
+#import <BBBAPI/BBAAuthenticationService.h>
+#import <BBBAPI/BBAConnection.h>
+#import <BBBAPI/BBACatalogueService.h>
+#import <BBBAPI/BBABookItem.h>
+#import <BBBAPI/BBAKeyService.h>
+#import <BBBAPI/BBAAuthenticationServiceConstants.h>
+#import <BBBAPI/BBAUserDetails.h>
+#import <BBBAPI/BBAClientDetails.h>
+#import <BBBAPI/BBAAuthData.h>
+#import <BBBAPI/BBALinkItem.h>
+#import <BBBAPI/BBAImageItem.h>
+#import <BBBAPI/BBASearchService.h>
+#import <BBBAPI/BBASuggestionItem.h>
+#import <BBBAPI/BBASearchItem.h>
